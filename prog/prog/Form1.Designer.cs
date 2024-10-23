e2e2f0fe9432a0de4289ef81d56effd7cae01e9b
unknown <haider55917@gmail.com>
2024-10-23 14:54:36 +0300

    adgg,,,,,,,,,,,

--- prog/prog/Form1.Designer.cs
+++ prog/prog/Form1.Designer.cs
@@ -60,7 +60,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(66, 42);
+            this.button1.Location = new System.Drawing.Point(66, 11);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(64, 116);
