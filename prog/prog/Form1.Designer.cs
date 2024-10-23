790e4c0a9a59c6c27015d9fb735d1a90e205e3db
unknown <haider55917@gmail.com>
2024-10-23 10:54:43 +0300

    adgg

--- prog/prog/Form1.Designer.cs
+++ prog/prog/Form1.Designer.cs
@@ -58,9 +58,9 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(274, 42);
+            this.dataGridView1.Location = new System.Drawing.Point(149, 42);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(471, 209);
+            this.dataGridView1.Size = new System.Drawing.Size(587, 221);
             this.dataGridView1.TabIndex = 2;
             // 
             // Form1
